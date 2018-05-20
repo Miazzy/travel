0ee6d89f995b0ee2500896cf9ed3494f05d7ed20
MicroIMOOC <1434246346@qq.com>
2018-05-20 16:49:03 +0800

    首页头部、轮播图开发

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <home-header />
+    <home-swiper />
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader,
+    HomeSwiper
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
+
+
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      <input type="text" placeholder="输入城市/景点">
+    </div>
+    <div class="header-right">
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~style/varibles.styl'
+.header
+  display flex
+  line-height $headerHeight
+  background $bgColor
+  color #fff
+  .header-left
+    width 0.64rem
+    float left
+    .back-icon
+      text-align center
+      font-size 0.4rem
+  .header-input
+    flex 1
+    height 0.64rem
+    line-height 0.64rem
+    margin-top 0.12rem
+    margin-left 0.2rem
+    padding-left 0.2rem
+    background #fff
+    border-radius 0.1rem
+    color #ccc
+  .header-right
+    min-width 1.04rem
+    padding 0 0.1rem
+    float right
+    text-align center
+    color #fff
+    .arrow-icon
+      margin-left -0.04rem
+      font-size 0.24rem
+</style>
