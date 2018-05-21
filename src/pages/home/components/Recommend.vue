3cd343b8fd96b6e1569c92fd5cb20e566261fe77
MicroIMOOC <1434246346@qq.com>
2018-05-21 15:45:50 +0800

    开发推荐、周末游组件

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -1,13 +1,77 @@
 <template>
-
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img :src="item.imgUrl" class="item-img">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: "HomeRecommend",
+  data() {
+    return {
+      recommendList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_140x140_73fda71d.jpg",
+          title: "大连圣亚海洋世界",
+          desc: "浪漫大连首站，浪漫的海洋主题乐园"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_140x140_73fda71d.jpg",
+          title: "大连圣亚海洋世界",
+          desc: "浪漫大连首站，浪漫的海洋主题乐园"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style lang="stylus" scoped>
-
+@import '~style/mixins.styl'
+.title
+  margin-top 0.2rem
+  line-height 0.8rem
+  background #eee
+  text-indent 0.2rem
+.item
+  overflow hidden
+  display flex
+  height 1.9rem
+  .item-img
+    width 1.7rem
+    height 1.7rem
+    padding 0.1rem
+  .item-info
+    flex 1
+    padding 0.1rem
+    min-width 0
+    .item-title
+      line-height 0.54rem
+      font-size 0.32rem
+      ellipsis()
+    .item-desc
+      line-height 0.4rem
+      color #ccc
+      ellipsis()
+    .item-button
+      line-height 0.44rem
+      margin-top 0.16rem
+      background #ff9300
+      padding 0 0.2rem
+      border-radius 0.06rem
+      color #fff
 </style>
